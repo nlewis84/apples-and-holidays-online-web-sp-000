620861416d347715f49d611b770dccc67b640124
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-26 17:54:33 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -97,7 +97,8 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  holiday_hash.include("BBQ")
+  binding.pry
 end
 
 
