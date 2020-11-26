4ece3c5440d79d5027c73a3ab25e9aff1363ebd6
Nathan Lewis <n.lewis.perc@gmail.com>
2020-11-26 17:52:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -89,7 +89,7 @@ def all_supplies_in_holidays(holiday_hash)
     holiday_hash.each do |season, holidays|
       puts "#{season.capitalize}:"
       holidays.each do |holiday_name, supply_array|
-        puts " #{holiday_name.to_s.split("_").map {|name| name.to_s.capitalize}.join(" ")}: #{supply_array.join(", ")}"
+        puts "  #{holiday_name.to_s.split("_").map {|name| name.to_s.capitalize}.join(" ")}: #{supply_array.join(", ")}"
       end
     end
 end
